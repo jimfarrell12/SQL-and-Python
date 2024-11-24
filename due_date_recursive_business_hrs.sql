20416f75901a095861ebc595c2f38015525a127d
jimfarrell12 <60685521+jimfarrell12@users.noreply.github.com>
2024-11-24 14:48:14 -0600

    Update due_date_recursive_business_hrs.sql

--- due_date_recursive_business_hrs.sql
+++ due_date_recursive_business_hrs.sql
@@ -33,7 +33,7 @@ create temp table sla_windows as --assign window and biz vs. stnd designation re
 		orderpriority
 	--priority
 		,case 
-			when orderpriority = 'Emergency 1-Hour' then 1/24::float
+			when orderpriority = 'Emergency 1' then 1/24::float
 			when orderpriority = 'Emergency 2' then 2/24::float
 			when orderpriority = 'Emergency 3' then 3/24::float
 			when orderpriority = 'Next-Day' then 1
